1c053d58a522002251c1177c841881db0698a471
Laminal Falah <laminalfalah08@gmail.com>
2023-11-23 16:37:35 +0700

    Remove enable web mvc

--- src/main/java/io/github/laminalfalah/eureka/EurekaDiscoveryApplication.java
+++ src/main/java/io/github/laminalfalah/eureka/EurekaDiscoveryApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @EnableEurekaServer
 @EnableDiscoveryClient
 @SpringBootApplication
